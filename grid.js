f3cf45b70dacfa1cbb765d1f6d816575241e6ee4
nathandeans <ndeans13@gmail.com>
2023-01-30 14:58:46 -0500

    Made 16x16 grid with hover effect

---
+++ grid.js
@@ -0,0 +1,20 @@
+
+
+let board = document.querySelector(".board")
+board.style.gridTemplateColumns ="repeat(16 , 1fr)"
+board.style.gridTemplateRows ="repeat(16, 1fr)"
+
+
+for(let i =0; i <256; i++) {
+    let square =document.createElement("div")
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    })
+    square.style.backgroundColor ="purple"
+    board.insertAdjacentElement("beforeend", square)
+}
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,84 @@
+const container = document.querySelector('#container');
+
+
+
+
+
+let square1 = document.createElement("div");
+let square2 = document.createElement("div");
+let square3 = document.createElement("div");
+let square4 = document.createElement("div");
+let square5 = document.createElement("div");
+let square6 = document.createElement("div");
+let square7 = document.createElement("div");
+let square8 = document.createElement("div");
+let square9 = document.createElement("div");
+let square10 = document.createElement("div");
+let square11 = document.createElement("div");
+let square12 = document.createElement("div");
+let square13 = document.createElement("div");
+let square14 = document.createElement("div");
+let square15 = document.createElement("div");
+let square16 = document.createElement("div");
+
+
+
+square1.className = "square"
+square2.className = "square"
+square3.className = "square"
+square4.className = "square"
+square5.className = "square"
+square6.className = "square"
+square7.className = "square"
+square8.className = "square"
+square9.className = "square"
+square10.className = "square"
+square11.className = "square"
+square12.className = "square"
+square13.className = "square"
+square14.className = "square"
+square15.className = "square"
+square16.className = "square"
+
+square1.setAttribute("id","divSquare");
+square2.setAttribute("id","divSquare");
+square3.setAttribute("id","divSquare");
+square4.setAttribute("id","divSquare");
+square5.setAttribute("id","divSquare");
+square6.setAttribute("id","divSquare");
+square7.setAttribute("id","divSquare");
+square8.setAttribute("id","divSquare");
+square9.setAttribute("id","divSquare");
+square10.setAttribute("id","divSquare");
+square11.setAttribute("id","divSquare");
+square12.setAttribute("id","divSquare");
+square13.setAttribute("id","divSquare");
+square14.setAttribute("id","divSquare");
+square15.setAttribute("id","divSquare");
+square16.setAttribute("id","divSquare");
+
+
+
+
+
+
+
+
+container.appendChild(square1)
+container.appendChild(square2)
+container.appendChild(square3)
+container.appendChild(square4)
+container.appendChild(square5)
+container.appendChild(square6)
+container.appendChild(square7)
+container.appendChild(square8)
+container.appendChild(square9)
+container.appendChild(square10)
+container.appendChild(square11)
+container.appendChild(square12)
+container.appendChild(square13)
+container.appendChild(square14)
+container.appendChild(square15)
+container.appendChild(square16)
+
+
